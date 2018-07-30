f36486a3adeb3160404ae05c206f597bc9c3f16b
btl <bertrand.thill@francetv.fr>
2018-07-30 09:56:39 +0200

    fix string / integer config

--- manifests/config.pp
+++ manifests/config.pp
@@ -14,23 +14,23 @@
 class thumbor::config {
   file {'/etc/default/thumbor':
     ensure => present,
-    owner  => root,
-    group  => root,
-    mode   => 0644,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
     content=> template('thumbor/default.erb'),
   }
   file {'/etc/thumbor.conf':
     ensure => present,
-    owner  => root,
-    group  => root,
-    mode   => 0644,
+    owner  =>'root',
+    group  => 'root',
+    mode   => '0644',
     content=> template($thumbor::conffile,'thumbor/thumbor.conf.erb')
   }
   file {'/etc/thumbor.key':
     ensure => present,
     owner  => 'thumbor',
     group  => 'thumbor',
-    mode   => 0600,
+    mode   => '0600',
     content=> $thumbor::security_key,
   }
 }
