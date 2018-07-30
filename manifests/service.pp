7b945b44275ac2c132845cf773ae4009ec36e0f1
btl <bertrand.thill@francetv.fr>
2018-07-30 10:43:46 +0200

    test sd

--- manifests/service.pp
+++ manifests/service.pp
@@ -11,15 +11,29 @@
 # == Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
+case $thumbor_service_provider {
+    'systemd': {
+      file { '/etc/systemd/system/thumbor-service.cfg':
+        ensure  => $newrelic_php_service_ensure,
+        content => template('thumbor/thumbor.service.erb'),
+      }~>
+      exec { 'thumbor-systemd-reload':
+        command     => 'systemctl daemon-reload',
+        path        => [ '/usr/bin', '/bin', '/usr/sbin', '/usr/local/bin/' ],
+        refreshonly => true,
+      }
+    }
+    default: {}
+  }
+
+
 class thumbor::service {
   service {"thumbor":
     require => File['/etc/thumbor.conf',
                     '/etc/thumbor.key',
                     '/etc/default/thumbor'],
-    ensure  => running,
-    enable  => true,
-    subscribe => File['/etc/thumbor.conf',
-                    '/etc/thumbor.key',
-                    '/etc/default/thumbor'],
+    provider   => thumbor_service_provider,
+    hasrestart => true,
+    hasstatus  => true,
   }
 }
